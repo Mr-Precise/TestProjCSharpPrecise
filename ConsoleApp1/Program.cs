1e90082f9b3387e96179f7ce2d4968a2921a3f7a
Mr-Precise <3824793+Mr-Precise@users.noreply.github.com>
2018-09-30 14:49:28 +0300

    example

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,12 @@ namespace MyTestProgram
     {
         static void Main(string[] args)
         {
+            int exampleB = 10;
+            float newfloat1 = 10.2F;
             Console.Title = "New Program";
             Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Hi, this is a new test program.");
+            Console.WriteLine(exampleB + newfloat1);
             Console.ReadKey();
         }
     }
